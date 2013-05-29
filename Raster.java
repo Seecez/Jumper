f814fc2c8e920a2453317d6183c25737044dd0c1
TheIceman2706 <mayfriedrich@gmail.com>
2013-05-29 11:32:47 +0200

    Makeing files java

---
+++ Raster.java
@@ -0,0 +1,13 @@
+public class Raster{
+
+public Raster(int pStartX, int pStartY, int pEndX, int EndY){
+int startX = pStartX;
+int startY = pStartY;
+int endX = pEndX;
+int endY = pEndY;
+
+
+}
+
+
+}
