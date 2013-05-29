f6945c1b497b87dd5d0a89d7efd24c63d4c277da
TheIceman2706 <mayfriedrich@gmail.com>
2013-05-29 11:31:58 +0200

    Makeing files java

---
+++ Rand.java
@@ -0,0 +1,22 @@
+public abstract class Rand{
+
+
+public Rand(int pJumperX, int pJumperY, int pDiffX, int pDiffY){
+
+int JumperX = pJumperX;
+int JumperY = pJumperY;
+
+int DiffX = pDiffX;
+int DiffY = pDiffY;
+
+}
+
+public boolean aendertX(){
+
+}
+
+public boolean aendertY(){
+
+}
+
+}
